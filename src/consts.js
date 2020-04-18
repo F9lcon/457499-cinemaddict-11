b171e097d34345ab2fb74604c5113b567a1c0f61
f9lcon <f9lcon320@gmail.com>
2020-04-18 18:44:26 +0300

    отделение данных от верстки

---
+++ src/consts.js
@@ -0,0 +1,21 @@
+export const MONTH_NAMES = [
+  `January`,
+  `February`,
+  `March`,
+  `April`,
+  `May`,
+  `June`,
+  `July`,
+  `August`,
+  `September`,
+  `October`,
+  `November`,
+  `December`,
+];
+
+export const EMOJI_LIST = [
+  `angry`,
+  `puke`,
+  `sleeping`,
+  `smile`
+].map((emoji) => `./images/emoji/${emoji}.png`);
--- src/main.js
+++ src/main.js
@@ -4,10 +4,11 @@ import {createFilmsContainer} from "./components/films-container";
 import {createCardFilm} from "./components/card-film";
 import {createBtnShowMore} from "./components/btn-show-more";
 import {createFilmDetails} from "./components/film-details";
+import {generateMock} from "./mock/film";
 
-const FILM_COUNT = 5;
-const EXTRA_FILM_COUNT = 2;
-
+const FILM_COUNT = 22;
+const SHOWING_FILMS_COUNT_ON_START = 5;
+const SHOWING_FILMS_COUNT_BY_BUTTON = 5;
 const mainElement = document.querySelector(`main`);
 
 const renderComponent = (container, template) => {
@@ -19,15 +20,24 @@ renderComponent(mainElement, createNavigator());
 renderComponent(mainElement, createFilmsContainer());
 
 const listContainer = mainElement.querySelector(`.films-list__container`);
-const extraList = mainElement.querySelectorAll(`.films-list--extra .films-list__container`);
 
-for (let i = 0; i < FILM_COUNT; i++) {
-  renderComponent(listContainer, createCardFilm());
-}
+const films = generateMock(FILM_COUNT);
 
-for (let i = 0; i < EXTRA_FILM_COUNT; i++) {
-  extraList.forEach((list) => renderComponent(list, createCardFilm()));
-}
+let showingFilmsCounter = SHOWING_FILMS_COUNT_ON_START;
+films.slice(0, showingFilmsCounter).forEach((film) => renderComponent(listContainer, createCardFilm(film)));
 
 renderComponent(mainElement.querySelector(`.films-list`), createBtnShowMore());
-renderComponent(document.querySelector(`body`), createFilmDetails());
+renderComponent(document.querySelector(`body`), createFilmDetails(films[0]));
+
+
+const showMoreBtn = document.querySelector(`.films-list__show-more`);
+showMoreBtn.addEventListener(`click`, () => {
+  const prevFilmCount = showingFilmsCounter;
+  showingFilmsCounter += SHOWING_FILMS_COUNT_BY_BUTTON;
+  films.slice(prevFilmCount, showingFilmsCounter).forEach((film) =>
+    renderComponent(listContainer, createCardFilm(film)));
+  if (showingFilmsCounter >= films.length) {
+    showMoreBtn.remove();
+  }
+});
+
